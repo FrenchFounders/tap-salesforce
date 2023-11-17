f07e6a104bbe6acec7bc2823433b2b6d3730f848
malikbemmansour <mbemmansour@gmail.com>
2023-11-17 11:03:23 +0100

    :feat: add limit 500 to content document link stream

--- tap_salesforce/streams.py
+++ tap_salesforce/streams.py
@@ -37,7 +37,7 @@ class ContentDocumentLinksStream(SalesforceStream):
         params["q"] = "SELECT Id,LinkedEntityId,ContentDocumentId,IsDeleted,SystemModstamp FROM ContentDocumentLink WHERE LinkedEntityId IN (SELECT Id FROM {})".format(self.source)
         if self.get_starting_replication_key_value(context) is not None:
             params["q"] += f" AND SystemModstamp>{self.get_starting_replication_key_value(context)[:19]}Z"
-        params["q"] += " ORDER BY SystemModstamp"
+        params["q"] += " ORDER BY SystemModstamp LIMIT 500"
         return params
 
     def prepare_request(self, context: dict | None, next_page_token=None):
